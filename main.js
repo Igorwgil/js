f8e1c4db929ee6d06dc6591fca4623283f38ad9a
Igor Daniel <igordaniel.wgil6@gmail.com>
2025-05-09 12:07:48 -0400

    finished

---
+++ main.js
@@ -0,0 +1,16 @@
+
+console.error('This an error');
+console.warn('Alert'); 
+
+let age = 30;
+age = 32;
+
+console.log(age);
+
+const score = 10;
+
+//reassign error
+
+score = 11;
+
+console.log(score);
